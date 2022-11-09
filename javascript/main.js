1bcc943dd83b0266427d954f87a7c58307bce4ed
Maino24 <mg02.gabriele@gmail.com>
2022-11-09 19:16:38 +0100

    select

--- javascript/main.js
+++ javascript/main.js
@@ -1,9 +1,36 @@
-/*console.log(`ciao`)*/
 
+let buttonHtml = document.getElementById(`btnInvio`)
+let inputName = document.getElementById(`inputName`)
+let inputKm = document.getElementById(`inputKm`)
+let inputEta = document.getElementById(`inputEta`)
+
+
+
+buttonHtml.addEventListener(`click`, function(){
+
+    const valueName = inputName.value;
+
+    const valueKm = inputKm.value;
+
+    const valueEta = inputEta.value;
+
+    if( valueName == `` && valueKm == `` && valueEta == ``){
+
+
+    }else{
+        
+    }
+
+
+
+})
+
+/*
 let input1 = document.getElementById(`inputKm`)
 let input2 = document.getElementById(`inputAge`)
 let input3 = document.getElementById(`inputName`)
-
+let numeroCp = document.getElementById(`numeroCp`)
+let carrozza = document.getElementById(`carrozza`)
 
 function esegui(){
 
@@ -40,20 +67,25 @@ if (Age < 18){
     console.log(finalPrice)
 }
 
-
+ 
+let numeroCp1 = Math.floor(Math.random() * 100000 ) +1;
+let carrozza1 = Math.floor(Math.random() * 100 ) +1;
 
 document.getElementById(`output`).innerHTML= `${finalPrice}`
 document.getElementById(`outputName`).innerHTML= `${Name}`
 //document.getElementsById(`numberRandomEs`).innerHTML= Math.floor(Math.random()*100000) + 1;
 
-}
+console.log(numeroCp1)
+document.getElementById(`numeroCp`).innerHTML = `${numeroCp1}`
+console.log(carrozza1)
+document.getElementById(`carrozza`).innerHTML = `${carrozza1}`
 
+}
 
 function functionAnnulla(){
     document.getElementsByClassName(`Annulla`).reset();
+    document.getElementById(`output`).reset();
+    document.getElementById(`outputName`).reset();
 }
 
-
-
-
- 
+*/
